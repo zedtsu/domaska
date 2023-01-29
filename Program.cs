f1c3e6da8a1b63160d32a4ab28d3a743cafb83b1
Sasha <sashok.ey@mail.ru>
2023-01-29 16:33:17 +0800

    gotovo

--- Program.cs
+++ Program.cs
@@ -1,1 +1,77 @@
-﻿
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.Clear();
+
+// Console.WriteLine("Введите 1 число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите 2 число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+//     Console.WriteLine($"Число {a} большее, а {b} меньшее.");
+// else if (b > a)
+//     Console.WriteLine($"Число {b} большее, а {a} меньшее.");
+// else if (a == b)
+//     Console.WriteLine("Числа равны.");
+
+
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.Clear();
+
+// Console.WriteLine("Введите 1 число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите 2 число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите 3 число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (b > max)
+// {
+//     max = b;
+//     Console.WriteLine(max);
+// }
+    
+// else if (c > max)
+// {
+//     max = c;
+//     Console.WriteLine(max);
+// }
+// else 
+//     Console.WriteLine(max);
+
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.Clear();
+
+// Console.WriteLine("Введите число: ");
+// int d = Convert.ToInt32(Console.ReadLine());
+
+// if (d % 2 == 0)
+//     Console.WriteLine("Число чётное.");
+// else
+//     Console.WriteLine("Число нечётное.");
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Clear();
+
+// int count = 2;
+
+// Console.WriteLine("Введите число:");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// while (count <= n)
+// {
+//     Console.Write($"{count} ");
+//     count += 2;
+// }
