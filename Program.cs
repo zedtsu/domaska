69ab01bc0bf431300df3ea4319f3f68842a05edc
Sasha <sashok.ey@mail.ru>
2023-02-08 15:34:01 +0800

    gotovofoa

--- Program.cs
+++ Program.cs
@@ -1,58 +1,71 @@
-﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 // Console.Clear();
 
-// Console.WriteLine("Введите 5-значное число: ");
+// Console.Write("Введите первое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int result = 1;
+// int i = 1;
+
+// while (i <= x)
+// {
+//     result = result * n;
+//     i++;
+// }
+// Console.WriteLine(result);
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// int number = ReadInt("Введите число: ");
+// int len = NumberLen(number);
+// SumNumbers(number, len);
+
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// // Функция подсчета цифр в числе
+// int NumberLen(int a)
+// {
+//     int index = 0;
+//     while (a > 0)
+//     {
+//         a /= 10;
+//         index++;
+//     }
+//     return index;
+// }
+
+// // Функция вывода суммы цифр в числе
+// void SumNumbers(int n, int len)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= len; i++)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     Console.WriteLine(sum);
+// }
+// 
+
+// Console.Clear();
+
+// Console.Write("Введите количество учеников: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 
-// while (n > 99999 || n < 10000)
+// Console.Write("Введите рост учеников по убыванию: ");
+// int [] array = {100};
+// int i = array.Length;
+
+// while (i <= array.Length)
 // {
-//   Console.WriteLine("Вы ошиблись/nВведите 5-значное число: ");
-//   n = Convert.ToInt32(Console.ReadLine());
+//     array[1] = Convert.ToInt32(Console.ReadLine());
+//     i++;
 // }
-
-// int n1 = n / 10000;
-// int n2 = n % 10;
-// if (n1 != n2)
-//   Console.WriteLine("Это число не палиндром.");
-// else 
-// {
-//   if (n1 == n2)
-//   {
-//     int n3 = (n / 1000) - n1 * 10;
-//     int n4 = (n / 10) % 10;
-//     if(n3 == n4)
-//       Console.WriteLine("Данное число палиндром.");
-//     else
-//       Console.WriteLine("Это число не палиндром.");
-//   }
-// }
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-
-// Console.Clear();
-
-// Console.WriteLine("Введите x1, y1, z1: ");
-// double x1 = Convert.ToDouble(Console.ReadLine());
-// double y1 = Convert.ToDouble(Console.ReadLine());
-// double z1 = Convert.ToDouble(Console.ReadLine());
-
-// Console.WriteLine("Введите x2, y2, z2: ");
-// double x2 = Convert.ToDouble(Console.ReadLine());
-// double y2 = Convert.ToDouble(Console.ReadLine());
-// double z2 = Convert.ToDouble(Console.ReadLine());
-
-// double result = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-// Console.WriteLine($"Результат: {Math.Round(result, 2)}");
-
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
-// Console.Clear();
-
-// Console.WriteLine("Введите число: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-// for (int i = 1; i <= n; i++)
-// {
-//     int result = i * i * i;
-//     Console.Write($"{result} ");
-// }
+// Console.WriteLine(array);
